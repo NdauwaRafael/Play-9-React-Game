2cf64cb82f61b95c0434bd59ee8c96817521c345
Raphael Karanja <rkaranja@cytonn.com>
2018-11-27 10:29:07 +0300

    display radom numbers from 1 to 9

--- src/components/Stars/Stars.js
+++ src/components/Stars/Stars.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 const Stars =  ()=> {
-    const noOfStars = 8;
+    const noOfStars = 1 + Math.floor(Math.random()*9);
     let stars = [];
     for(let i = 0; i<noOfStars; i++) {
         stars.push(<i key={i} className="fa fa-star"> </i>)
